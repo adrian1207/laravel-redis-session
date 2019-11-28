e15346dee9487b875ae8c39a3df8b10eb47aa6e9
Adrian Mikołajczyk <a.mikolajczyk@ideo.pl>
2019-11-28 11:59:27 +0100

    Package initiation

---
+++ README.md
@@ -0,0 +1,3 @@
+# laravel-redis-session
+
+Package for node to get and set laravel session on redis driver.
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const serialize = require('php-serialization').serialize;
+const unserialize = require('php-serialization').unserialize;
+
+module.exports = {
+    
+};
